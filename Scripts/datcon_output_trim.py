ca95a4ab4b75928d12001c4b8af2ce965f58a53e
Peter <43998116+PetercSnk@users.noreply.github.com>
2022-03-08 19:31:41 +0000

    range added

--- Scripts/datcon_output_trim.py
+++ Scripts/datcon_output_trim.py
@@ -59,22 +59,32 @@ for x in range(len(files)):
     except:
         stats_dict["Time Zeros"] = 0
     # convert dataframe to numpy array
-    selection_array = (selection["GPS:Long"].to_numpy())
+    long_array = (selection["GPS:Long"].to_numpy())
+    lat_array = (selection["GPS:Lat"].to_numpy())
     # remove zeros
-    remove_zeros = selection_array[selection_array != 0] 
+    long_remove_zeros = long_array[long_array != 0] 
+    lat_remove_zeros = lat_array[lat_array != 0] 
     # remove nan value
-    remove_nan = remove_zeros[np.logical_not(np.isnan(remove_zeros))]
+    long_remove_nan = long_remove_zeros[np.logical_not(np.isnan(long_remove_zeros))]
+    lat_remove_nan = lat_remove_zeros[np.logical_not(np.isnan(lat_remove_zeros))]
     # find minmia and maxima values
-    minima = np.min(remove_nan)
-    maxima = np.max(remove_nan)
-    print(minima, maxima)
-    range = maxima - minima
-    if range > 1:
+    long_minima = np.min(long_remove_nan)
+    long_maxima = np.max(long_remove_nan)
+    lat_minima = np.min(lat_remove_nan)
+    lat_maxima = np.max(lat_remove_nan)
+    # calculate range
+    long_range = long_maxima - long_minima
+    lat_range = lat_maxima - lat_minima
+    # set warning if range is large
+    if long_range > 1:
         long_warning = True
     else:
         long_warning = False
+    if lat_range > 1:
+        lat_warning = True
+    else:
+        lat_warning = False
     # create log file to make identifing poor quality FLYXXX.DAT files easier
-    
     with open("datcon_output_trim/datcon_output_trim_log.txt", "a") as f:
         print(files[x], file = f)
         print("--------------------", file = f)
@@ -83,8 +93,11 @@ for x in range(len(files)):
         print("total rows : ", num_rows, file = f)
         if long_warning:
             print("WARNING large change in longitude", file = f)
-            print("longitude range ")
+            print("longitude range : ", long_range, file = f)
+        if lat_warning:
+            print("WARNING large change in latitude", file = f)
+            print("longitude range : ", lat_range, file = f)
         print("--------------------", file = f)
         
     # output selections to csv for QGIS
-    #selection.to_csv(f"datcon_output_trim/trim_{files[x]}")
+    selection.to_csv(f"datcon_output_trim/trim_{files[x]}")
